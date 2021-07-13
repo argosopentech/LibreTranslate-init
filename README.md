9379e37266b4783577a72bab7aca6a9655b04405
P.J. Finlay <pjappdevelopment@gmail.com>
2021-07-13 00:21:25 +0000

    Nginx and systemd setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LibreTranslate-init
 Run LibreTranslate on Ubuntu 20.04 server
+
+Runs [LibreTranslate with Gunicorn and Nginx](https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04).
--- setup.sh
+++ setup.sh
@@ -7,3 +7,15 @@ sudo apt-get install -y libicu-dev python3-icu pkg-config
 
 # Install Nginx
 sudo apt-get install -y nginx
+
+# Setup systemd
+sudo cp libretranslate.service /etc/systemd/system/
+sudo systemctl start libretranslate
+sudo systemctl enable libretranslate
+sudo systemctl status libretranslate
+
+# Configure Nginx
+sudo cp nginx /etc/nginx/sites-available/default
+sudo nginx -t
+sudo systemctl restart nginx
+
