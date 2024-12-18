ac0c8382bdee2caa007d41c71bfe55d100e3ced3
Ivan Davydov <lotigara@lotigara.ru>
2024-12-18 21:17:23 +0300

    Switch to certbot from APT

--- get-cert.sh
+++ get-cert.sh
@@ -1,9 +1,7 @@
 # Setup TLS with certbot and Let's Encrypt
 # https://certbot.eff.org/
 
-sudo snap install core; sudo snap refresh core
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo apt install certbot python3-certbot-nginx
 sudo certbot --nginx
 sudo certbot renew --dry-run
 
